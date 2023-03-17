6764e3c3676c96b8415eb8604711eac44655cf21
Arguelless <marguelles@protonmail.com>
2023-03-17 20:18:16 +0100

    Primer commit

---
+++ src/paquete_juego/Principal.java
@@ -0,0 +1,25 @@
+package paquete_juego;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Principal {
+    private static String nickname = "";
+
+    private final String[] nomsPokemon = {"Charmander", "Salamance", "Pikachu", "Treeko", "Torchi", "Rayquaza", "Milotic",
+            "Absol", "Garchomp", "Electrivire", "Empoleon", "Dialga", "Bronzong"};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean salir = false;
+
+        while (!salir){
+            System.out.println("Bienvenido a la región de Kanto. Para continuar escribe tu nombre: ");
+
+
+            }
+        }
+    }
+
+
+}
